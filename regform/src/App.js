645f86839c1b778b9f01d8b7ea4df0485d9cbace
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-06-06 12:29:53 -0700

    added registration form with validation

---
+++ regform/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './registrationForm/registrationForm'
+
+function App() {
+  return (
+    <div className="App">
+    <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
